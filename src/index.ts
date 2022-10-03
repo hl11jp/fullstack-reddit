0d398afa257d7a732eb5f8a8d3dd5ce35102468f
hl11jp <nguyenducthien9@gmail.com>
2022-10-04 00:00:40 +0700

    grapql setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
+
+  const app = express();
+  app.get("/", (_, res) => {
+    res.send("hello");
+  });
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(3000, () => {
+    console.log("listening on port 3000...");
+  });
   // const post = orm.em.create(Post, {title: 'omg', createdAt: new Date(), updatedAt: new Date()});
   // const post = orm.em.create(Post, {title: 'bro'}); //this will auto create Date
   // await orm.em.persistAndFlush(post);
