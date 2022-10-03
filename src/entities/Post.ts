921418b58c88fe793fccfbc1606766c52907388c
hl11jp <nguyenducthien9@gmail.com>
2022-10-04 00:26:15 +0700

    Convert entities to graphql object

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
 
+@ObjectType() //convert orm to graphql object
 @Entity()
 export class Post {
+  @Field(() => Int) //needed this field
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property({type: "date"})
   createdAt = new Date();
 
+  @Field(() => String)
   @Property({type: "date", onUpdate: () => new Date()})
   updatedAt = new Date();
 
+  @Field(() => String)
   @Property({type: "text"})
   title!: string;
 }
