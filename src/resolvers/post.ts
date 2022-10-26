5685eb3d01fd8b6e8d1f9af9be8f99b54563d1a9
hl11jp <nguyenducthien9@gmail.com>
2022-10-26 10:39:02 +0700

    Change post's id type to Int

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -101,8 +101,8 @@ export class PostResolver {
   }
 
   @Query(() => Post, { nullable: true })
-  post(@Arg("id") id: number): Promise<Post | null> {
-    return Post.findOneBy({ id });
+  post(@Arg("id", () => Int) id: number): Promise<Post | null> {
+    return Post.findOne({where: {id}, relations: ['creator']});
   }
 
   @Mutation(() => Post)
